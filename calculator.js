2f50736ec217ce2bf6ab21961aa68eef42cd3064
lewisjs42 <lewisjs42@gmail.com>
2018-12-12 17:21:10 -0500

    Added scaling

--- calculator.js
+++ calculator.js
@@ -7,6 +7,12 @@
 // - Add keyboard support
 // - Make it look nice
 
+//For scaling up on mobile
+var siteWidth = 325;
+var scale = screen.width / siteWidth
+
+document.querySelector('meta[name="viewport"]').setAttribute('content', 'width='+siteWidth+', initial-scale='+scale+'');
+
 function add(firstNumber, secondNumber) {
 	return firstNumber + secondNumber;
 }
